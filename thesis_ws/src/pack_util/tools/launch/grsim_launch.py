78cbd55af895949bb8f6f1adc2135cbd52558db8
Kian Behzad <kian.behzad@gmail.com>
2020-12-23 16:39:10 +0330

    [pack_util] update grsim launch file with new package names

--- thesis_ws/src/pack_util/tools/launch/grsim_launch.py
+++ thesis_ws/src/pack_util/tools/launch/grsim_launch.py
@@ -5,10 +5,10 @@ import os
 
 
 def generate_launch_description():
-    param_path = os.path.join(get_package_share_directory('parsian_util'), 'params', 'config_params.yaml')
+    param_path = os.path.join(get_package_share_directory('pack_util'), 'params', 'config_params.yaml')
     
-    grsim_node = Node(package='parsian_protobuf_wrapper', executable='grsim', name='grsim_node', parameters=[param_path])
-    vision_node = Node(package='parsian_protobuf_wrapper', executable='vision', name='vision_node', parameters=[param_path])
-    worldmodel_node = Node(package='parsian_world_model', executable='worldmodel', name='worldmodel_node', parameters=[param_path])
+    grsim_node = Node(package='pack_protobuf_wrapper', executable='grsim', name='grsim_node', parameters=[param_path])
+    vision_node = Node(package='pack_protobuf_wrapper', executable='vision', name='vision_node', parameters=[param_path])
+    worldmodel_node = Node(package='pack_world_model', executable='worldmodel', name='worldmodel_node', parameters=[param_path])
     
     return LaunchDescription([grsim_node, vision_node, worldmodel_node])
