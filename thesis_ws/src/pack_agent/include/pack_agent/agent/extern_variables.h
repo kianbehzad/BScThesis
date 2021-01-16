c8c04ae6341752c7a062bd7d05aa3e94a9f073e2
Kian Behzad <kian.behzad@gmail.com>
2021-01-16 17:17:44 +0330

    [pack_agent] dont need pid for velocity, replace all the node parameters with PID_angle instead of PID_vel

--- thesis_ws/src/pack_agent/include/pack_agent/agent/extern_variables.h
+++ thesis_ws/src/pack_agent/include/pack_agent/agent/extern_variables.h
@@ -8,11 +8,12 @@
 #include "pack_msgs/msg/world_model.hpp"
 
 extern pack_msgs::msg::WorldModel::SharedPtr extern_wm;
-extern double extern_I_vel;
-extern double extern_D_vel;
 extern double extern_P_pos;
 extern double extern_I_pos;
 extern double extern_D_pos;
+extern double extern_P_angle;
+extern double extern_I_angle;
+extern double extern_D_angle;
 extern double extern_max_vel;
 
 #endif //PACK_AGENT_EXTERN_VARIABLES_H
--- thesis_ws/src/pack_agent/src/agent/agent_node.cpp
+++ thesis_ws/src/pack_agent/src/agent/agent_node.cpp
@@ -6,12 +6,12 @@
 
 // extern value definitions
 pack_msgs::msg::WorldModel::SharedPtr extern_wm;
-double extern_P_vel = 1;
-double extern_I_vel = 0;
-double extern_D_vel = 0;
 double extern_P_pos = 1;
 double extern_I_pos = 0;
 double extern_D_pos = 0;
+double extern_P_angle = 1;
+double extern_I_angle = 0;
+double extern_D_angle = 0;
 double extern_max_vel = 4;
 
 AgentNode::AgentNode(const rclcpp::NodeOptions & options) : Node("agent_node", options)
@@ -31,9 +31,9 @@ AgentNode::AgentNode(const rclcpp::NodeOptions & options) : Node("agent_node", o
     parameter_event_sub = parameters_client->on_parameter_event(params_change_callback);
     
     // get parameter initial values
-    extern_P_vel = parameters_client->get_parameter("P_vel", extern_P_vel);
-    extern_I_vel = parameters_client->get_parameter("I_vel", extern_I_vel);
-    extern_D_vel = parameters_client->get_parameter("D_vel", extern_D_vel);
+    extern_P_angle = parameters_client->get_parameter("P_angle", extern_P_angle);
+    extern_I_angle = parameters_client->get_parameter("I_angle", extern_I_angle);
+    extern_D_angle = parameters_client->get_parameter("D_angle", extern_D_angle);
     extern_P_pos = parameters_client->get_parameter("P_pos", extern_P_pos);
     extern_I_pos = parameters_client->get_parameter("I_pos", extern_I_pos);
     extern_D_pos = parameters_client->get_parameter("D_pos", extern_D_pos);
@@ -68,20 +68,20 @@ void AgentNode::define_params_change_callback_lambda_function()
             //do stuff
         }
         for (auto & changed_parameter : event->changed_parameters) {
-            if(changed_parameter.name == "P_vel")
+            if(changed_parameter.name == "P_angle")
             {
-                extern_P_vel = changed_parameter.value.double_value;
-                if(extern_P_vel == 0)  extern_P_vel = changed_parameter.value.integer_value;//double_value gives 0 if the input has no decimals
+                extern_P_angle = changed_parameter.value.double_value;
+                if(extern_P_angle == 0)  extern_P_angle = changed_parameter.value.integer_value;//double_value gives 0 if the input has no decimals
             }
-            else if(changed_parameter.name == "I_vel")
+            else if(changed_parameter.name == "I_angle")
             {
-                extern_I_vel = changed_parameter.value.double_value;
-                if(extern_I_vel == 0)  extern_I_vel = changed_parameter.value.integer_value;//double_value gives 0 if the input has no decimals
+                extern_I_angle = changed_parameter.value.double_value;
+                if(extern_I_angle == 0)  extern_I_angle = changed_parameter.value.integer_value;//double_value gives 0 if the input has no decimals
             }
-            else if(changed_parameter.name == "D_vel")
+            else if(changed_parameter.name == "D_angle")
             {
-                extern_D_vel = changed_parameter.value.double_value;
-                if(extern_D_vel == 0)  extern_D_vel = changed_parameter.value.integer_value;//double_value gives 0 if the input has no decimals
+                extern_D_angle = changed_parameter.value.double_value;
+                if(extern_D_angle == 0)  extern_D_angle = changed_parameter.value.integer_value;//double_value gives 0 if the input has no decimals
             }
             else if(changed_parameter.name == "P_pos")
             {
