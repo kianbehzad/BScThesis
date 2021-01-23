f6780b20a4b827192266050b447e35716d908cee
Kian Behzad <kian.behzad@gmail.com>
2021-01-23 14:03:33 +0330

    [pack_world_model][simple_worldmodel_node] use simple_world_model node instead of the legacy world_model node in launch files

--- thesis_ws/src/pack_util/tools/launch/grsim_launch_noGUI.py
+++ thesis_ws/src/pack_util/tools/launch/grsim_launch_noGUI.py
@@ -9,7 +9,7 @@ def generate_launch_description():
     
     grsim_node = Node(package='pack_protobuf_wrapper', executable='grsim', name='grsim_node', parameters=[param_path])
     vision_node = Node(package='pack_protobuf_wrapper', executable='vision', name='vision_node', parameters=[param_path])
-    worldmodel_node = Node(package='pack_world_model', executable='worldmodel', name='worldmodel_node', parameters=[param_path])
+    worldmodel_node = Node(package='pack_world_model', executable='simple_worldmodel', name='worldmodel_node', parameters=[param_path])
     agent_param_node = Node(package='pack_agent', executable='agent_param', name='agent_param_node', parameters=[param_path])
     #interface_node = Node(package='pack_gui', executable='interface', name='interface_node', parameters=[param_path])
 
