a2047fdca97bc46a469fac354d8a4d533d4ca710
Kian Behzad <kian.behzad@gmail.com>
2021-02-27 13:50:22 +0330

    [pack_agent][gotopointAvoid] implementation of the basis of a good potential field

--- thesis_ws/src/pack_agent/src/agent/skill/skill_gotopoint_avoid.cpp
+++ thesis_ws/src/pack_agent/src/agent/skill/skill_gotopoint_avoid.cpp
@@ -16,7 +16,59 @@ SkillGotoPointAvoid::~SkillGotoPointAvoid()
 
 pack_msgs::msg::RobotCommand SkillGotoPointAvoid::execute(const pack_msgs::msg::Skill& skill)
 {
+    // initial assignments
+    skill_gotopointavoid_msg = skill.skill_gotopoint_avoid;
+    id = skill.id;
     pack_msgs::msg::RobotCommand robot_command;
+    robot_command.robot_id = id;
+
+    // find the desierd robot
+    pack_msgs::msg::Robot robot;
+    bool found_robot = false;
+    for (const auto& bot : extern_wm->our)
+        if (bot.id == id)
+        {   robot = bot; found_robot = true; }
+    if (!found_robot)
+    {   qDebug() << "[agent_node] robot id out of range (no id=" << id << " found)"; return robot_command; }
+
+
+    // attraction force
+    rcsc::Vector2D destination_vec = robot.pos - rcsc::Vector2D{skill_gotopointavoid_msg.destination};
+    double att_rad = extern_attraction_radius;
+    double att_step = extern_attraction_step;
+    rcsc::Vector2D attraction = (destination_vec.length() <= att_rad) ? -att_step * destination_vec : -destination_vec.length()*att_step*destination_vec/att_rad;
+
+    // repulsion force
+    rcsc::Vector2D obstacle_center{0, 0};
+    double rep_rad = extern_repulsion_radius;
+    double rep_step = extern_repulsion_step;
+    rcsc::Vector2D robot_obstacle_vec = (rcsc::Vector2D(robot.pos) + rcsc::Vector2D(robot.vel)*0.4) - obstacle_center;
+    rcsc::Vector2D repulsion = (robot_obstacle_vec.length() <= rep_rad) ? rep_step*(1/robot_obstacle_vec.length() - 1/rep_rad)*(1/(robot_obstacle_vec.length()*robot_obstacle_vec.length()))*(robot_obstacle_vec/robot_obstacle_vec.length()) : rcsc::Vector2D{0, 0};
+
+    rcsc::Vector2D final_force = attraction + repulsion;
+    if (final_force.length() > extern_max_vel) final_force.setLength(extern_max_vel);
+
+    rcsc::Vector2D robot_dir = rcsc::Vector2D{robot.dir}.norm();
+    rcsc::Vector2D robot_norm_dir = robot_dir.rotatedVector(90);
+
+    double velf = (final_force.x*robot_norm_dir.y - final_force.y*robot_norm_dir.x) / (robot_dir.x*robot_norm_dir.y - robot_dir.y*robot_norm_dir.x);
+    double veln = (final_force.y-velf*robot_dir.y)/(robot_norm_dir.y);
+
+    extern_drawer->choose_pen("darkgray", false);
+    extern_drawer->draw_line(robot.pos, rcsc::Vector2D(skill_gotopointavoid_msg.destination));
+
+    extern_drawer->choose_pen("red", true);
+    extern_drawer->draw_radial_gradient(obstacle_center, rep_rad);
+
+    extern_drawer->choose_pen("blue", false);
+    extern_drawer->draw_circle(robot.pos, rcsc::Vector2D(robot.vel).length()*0.25);
+
+
+    // fill the robot command message
+    robot_command.vel_f = velf;
+    robot_command.vel_n = veln;
+    robot_command.vel_w = 0;
+    robot_command.wheels_speed = false;
 
     return robot_command;
 }
