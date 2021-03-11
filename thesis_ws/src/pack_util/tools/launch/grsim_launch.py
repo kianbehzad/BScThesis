a1314d60f2bf07f74eb741f520bebd3aad4a2830
Kian Behzad <kian.behzad@gmail.com>
2021-03-11 20:34:29 +0330

    [pack_util][launch] add 5 robots' agent node (id:0-4)

--- thesis_ws/src/pack_util/tools/launch/grsim_launch.py
+++ thesis_ws/src/pack_util/tools/launch/grsim_launch.py
@@ -11,7 +11,12 @@ def generate_launch_description():
     vision_node = Node(package='pack_protobuf_wrapper', executable='vision', name='vision_node', parameters=[param_path])
     worldmodel_node = Node(package='pack_world_model', executable='simple_worldmodel', name='worldmodel_node', parameters=[param_path])
     agent_param_node = Node(package='pack_agent', executable='agent_param', name='agent_param_node', parameters=[param_path])
+    agent_0 = Node(package='pack_agent', executable='agent', name='agent_0', parameters=[param_path])
+    agent_1 = Node(package='pack_agent', executable='agent', name='agent_1', parameters=[param_path])
+    agent_2 = Node(package='pack_agent', executable='agent', name='agent_2', parameters=[param_path])
+    agent_3 = Node(package='pack_agent', executable='agent', name='agent_3', parameters=[param_path])
+    agent_4 = Node(package='pack_agent', executable='agent', name='agent_4', parameters=[param_path])
     interface_node = Node(package='pack_gui', executable='interface', name='interface_node', parameters=[param_path])
 
-    return LaunchDescription([grsim_node, vision_node, worldmodel_node, interface_node, agent_param_node])
+    return LaunchDescription([grsim_node, vision_node, worldmodel_node, interface_node, agent_param_node, agent_0, agent_1, agent_2, agent_3, agent_4])
 
