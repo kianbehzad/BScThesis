41084adfbc478a342eae59bc83b789502f4130ac
Kian Behzad <kian.behzad@gmail.com>
2021-03-13 16:59:15 +0330

    [pack_ai] add push_ball function to push the ball yo a specific destination

--- thesis_ws/src/pack_ai/include/pack_ai/ai/coach.h
+++ thesis_ws/src/pack_ai/include/pack_ai/ai/coach.h
@@ -5,6 +5,9 @@
 #ifndef PACK_AI_COACH_H
 #define PACK_AI_COACH_H
 
+#include "pack_util/core/field.h"
+#include "pack_util/core/knowledge.h"
+#include "pack_util/geom/segment_2d.h"
 #include "pack_ai/ai/extern_variables.h"
 
 #include <QDebug>
@@ -17,8 +20,13 @@ public:
     void execute();
 
 private:
+    CField field;
+
     // gets the desired id and returns the index corresponding to the id in wm.our
     int ID(int id);
+
+    // push ball to a destination
+    void push_ball(const int& id, const rcsc::Vector2D& destination);
 };
 
 #endif //PACK_AI_COACH_H
--- thesis_ws/src/pack_ai/src/ai/coach.cpp
+++ thesis_ws/src/pack_ai/src/ai/coach.cpp
@@ -10,7 +10,7 @@ Coach::~Coach() = default;
 
 void Coach::execute()
 {
-    qDebug() << ID(3) << " - " << extern_wm->our[ID(3)].id;
+    push_ball(0, field.oppGoal());
 }
 
 int Coach::ID(int id)
@@ -20,3 +20,19 @@ int Coach::ID(int id)
             return i;
     return -1;
 }
+
+void Coach::push_ball(const int& id, const rcsc::Vector2D& destination)
+{
+    rcsc::Vector2D robot_pos = extern_wm->our[ID(id)].pos;
+    rcsc::Vector2D ball_pos = extern_wm->ball.pos + extern_wm->ball.vel*0.3;
+    rcsc::Vector2D ball_destination = (ball_pos - destination).norm();
+    rcsc::Vector2D position = ball_pos + ball_destination*(knowledge::ROBOT_RADIUS + 0.05);
+    Segment2D seg{robot_pos, position};
+    if (robot_pos.dist(position) < 0.1)
+        extern_skill_handler->direct_velocity(id, -ball_destination*0.5, destination);
+    else if (seg.dist(ball_pos) < 0.09)
+        extern_skill_handler->gotopoint_avoid(id, position, destination, true);
+    else
+        extern_skill_handler->gotopoint(id, position, destination);
+}
+
